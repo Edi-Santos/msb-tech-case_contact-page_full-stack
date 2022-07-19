6c5df46f3815c3e5e6603e6bca688b7272be7942
Ed Santos <edsantos2822@gmail.com>
2022-07-19 01:05:33 -0300

    feat:(app.js): criando endpoint POST /contact

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const Contact = require('../controller/Contact');
+
 const app = express();
 app.use(express.json());
+
+app.post('/contact', Contact.addingContact);
 
 module.exports = app;
