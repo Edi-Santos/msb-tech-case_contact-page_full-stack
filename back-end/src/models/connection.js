932cd530b5c6ae61ea7c45aa56ed32ad9a7ed218
Ed Santos <edsantos2822@gmail.com>
2022-07-19 00:23:37 -0300

    chore:(connection.js): mudando nome do banco de dados

--- back-end/src/models/connection.js
+++ back-end/src/models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
-const DB_NAME = 'PSC_msb-tech';
+const DB_NAME = 'PSC_msb-tech_contact';
 
 const OPTIONS = {
   useNewUrlParser: true,
