c77f27404e15421c2f91e9426234eb1d0980aa77
Ed Santos <edsantos2822@gmail.com>
2022-07-20 01:28:38 -0300

    test:(contact.test.js): adicionando testes ao endpoint em caso de falha - não passado propriedade "email" ou "email" inválido

--- back-end/tests/contact.test.js
+++ back-end/tests/contact.test.js
@@ -103,4 +103,64 @@ describe('POST /contact', () => {
       expect(response.body.message).to.be.equal('"name" is not allowed to be empty');
     });
   });
+
+  describe('Testa quando a requisição é mal sucedida - Não é passado "email" ou "email" inválido', () => {
+    let response1 = {};
+    let response2 = {};
+    
+    before(async () => {
+      const DBServer = await MongoMemoryServer.create();
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(URLMock,
+        {useNewUrlParser: true, useUnifiedTopology: true},
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      response1 = await chai.request(server)
+        .post('/contact')
+        .send({
+          "name": "Eren Yeager",
+          "email": "",
+          "cellphone": "(21) 99999-9999",
+          "message": "Teste de qualidade"
+        });
+
+        response2 = await chai.request(server)
+        .post('/contact')
+        .send({
+          "name": "Eren Yeager",
+          "email": "erengmail.com",
+          "cellphone": "(21) 99999-9999",
+          "message": "Teste de qualidade"
+        });
+
+      await DBServer.stop();
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+    });
+
+    it('retorna o código de status 400', () => {
+      expect(response1).to.have.status(400);
+      expect(response2).to.have.status(400);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response1.body).to.be.a('object');
+      expect(response2.body).to.be.a('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response1.body).to.have.property('message');
+      expect(response2.body).to.have.property('message');
+    });
+
+    it('a propriedade "message" precisa ter o texto "\"email\" is not allowed to be empty" ou \"email\" must be a valid email', () => {
+      expect(response1.body.message).to.be.equal('"email" is not allowed to be empty');
+      expect(response2.body.message).to.be.equal('"email" must be a valid email');
+    });
+  });
 });
