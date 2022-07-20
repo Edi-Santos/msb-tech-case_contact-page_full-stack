1fee8b8290e9c8ce1be4ccb10a2429c9a2701a27
Ed Santos <edsantos2822@gmail.com>
2022-07-20 01:53:48 -0300

    test:(contact.test.js): adicionando testes ao endpoint em caso de falha - não passado propriedade "cellphone" ou "cellphone" inválido

--- back-end/tests/contact.test.js
+++ back-end/tests/contact.test.js
@@ -163,4 +163,66 @@ describe('POST /contact', () => {
       expect(response2.body.message).to.be.equal('"email" must be a valid email');
     });
   });
+
+  describe('Testa quando a requisição é mal sucedida - Não é passado "cellphone" ou "cellphone" inválido', () => {
+    let response1 = {};
+    let response2 = {};
+    
+    before(async () => {
+      const DBServer = await MongoMemoryServer.create();
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(URLMock,
+        {useNewUrlParser: true, useUnifiedTopology: true},
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      response1 = await chai.request(server)
+        .post('/contact')
+        .send({
+          "name": "Eren Yeager",
+          "email": "eren@gmail.com",
+          "cellphone": "",
+          "message": "Teste de qualidade"
+        });
+
+        response2 = await chai.request(server)
+        .post('/contact')
+        .send({
+          "name": "Eren Yeager",
+          "email": "eren@gmail.com",
+          "cellphone": "(21) 999-9999",
+          "message": "Teste de qualidade"
+        });
+
+      await DBServer.stop();
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+    });
+
+    it('retorna o código de status 400', () => {
+      expect(response1).to.have.status(400);
+      expect(response2).to.have.status(400);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response1.body).to.be.a('object');
+      expect(response2.body).to.be.a('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response1.body).to.have.property('message');
+      expect(response2.body).to.have.property('message');
+    });
+
+    it('a propriedade "message" precisa ter o texto "\"cellphone\" is not allowed to be empty" ou \"cellphone\" with value...', () => {
+      const expectedValueRes2 = "\"cellphone\" with value \"(21) 999-9999\" fails to match the required pattern: /^(?:(?:\\+|00)?(55)\\s?)?(?:\\(?([1-9][0-9])\\)?\\s?)?(?:((?:9\\d|[2-9])\\d{3})-?(\\d{4}))$/"
+  
+      expect(response1.body.message).to.be.equal('"cellphone" is not allowed to be empty');
+      expect(response2.body.message).to.be.equal(expectedValueRes2);
+    });
+  });
 });
