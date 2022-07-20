0dc92b411b3d506320c43428ada6cc66266859f5
Ed Santos <edsantos2822@gmail.com>
2022-07-20 01:21:23 -0300

    test:(contact.test.js): adicionando testes ao endpoint em caso de falha - não passado propriedade "name"

--- back-end/tests/contact.test.js
+++ back-end/tests/contact.test.js
@@ -57,4 +57,50 @@ describe('POST /contact', () => {
       expect(response.body.message).to.be.equal('Contato enviado com sucesso');
     });
   });
+
+  describe('Testa quando a requisição é mal sucedida - Não é passado "name"', () => {
+    let response = {};
+    
+    before(async () => {
+      const DBServer = await MongoMemoryServer.create();
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(URLMock,
+        {useNewUrlParser: true, useUnifiedTopology: true},
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      response = await chai.request(server)
+        .post('/contact')
+        .send({
+          "name": "",
+          "email": "eren@gmail.com",
+          "cellphone": "(21) 99999-9999",
+          "message": "Teste de qualidade"
+        });
+
+      await DBServer.stop();
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+    });
+
+    it('retorna o código de status 400', () => {
+      expect(response).to.have.status(400);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.a('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response.body).to.have.property('message');
+    });
+
+    it('a propriedade "message" precisa ter o texto "\"name\" is not allowed to be empty"', () => {
+      expect(response.body.message).to.be.equal('"name" is not allowed to be empty');
+    });
+  });
 });
