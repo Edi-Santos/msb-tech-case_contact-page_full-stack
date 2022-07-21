6b71fe873f934ffe90ac63dcaa579609862f7a2a
Ed Santos <edsantos2822@gmail.com>
2022-07-21 15:47:13 -0300

    build: habilitando requisição com o cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const Contact = require('../controller/Contact');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/contact', Contact.addingContact);
 
