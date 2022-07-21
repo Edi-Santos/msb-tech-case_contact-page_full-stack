7d34355c89dc660029ca6edd53954f2d15a6bebe
Ed Santos <edsantos2822@gmail.com>
2022-07-21 07:07:26 -0300

    feat: adicionando rota e redirecionamento para página <Home />

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>Meu pastel é mais barato</>
+    <Routes>
+      <Route path="/home" element={ <Home /> } />
+      <Route path="/" element={ <Navigate replace to="/home" /> } />
+    </Routes>
   );
 }
 
